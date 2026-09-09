e0271dd491645d6fbe63c6ddc041f83e8209ef05
Elikto <105332200+Elikto@users.noreply.github.com>
2026-09-10 01:54:04 +0200

    Initialize account progress synchronization

--- app/src/main/java/com/example/cyberquiz/CyberQuizApplication.kt
+++ app/src/main/java/com/example/cyberquiz/CyberQuizApplication.kt
@@ -2,6 +2,7 @@ package com.example.cyberquiz
 
 import android.app.Application
 import com.example.cyberquiz.engagement.EngagementStore
+import com.example.cyberquiz.social.ProgressSyncManager
 import com.example.cyberquiz.update.CyberQuizUpdateNotificationManager
 import com.example.cyberquiz.update.CyberQuizUpdateNotificationScheduler
 
@@ -15,6 +16,7 @@ class CyberQuizApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         EngagementStore.recordDailyLogin(this)
+        ProgressSyncManager.initialize(this)
         CyberQuizUpdateNotificationManager.createNotificationChannel(this)
         CyberQuizUpdateNotificationScheduler.schedule(this)
     }
